c1f558dbd55b7084187c281e720ec862bdd09e7e
Anurag Chaurasiya <anurag9120959628@gmail.com>
2025-12-12 23:05:41 +0530

    add port in db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,8 +7,13 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const db = await mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: isProd ? { rejectUnauthorized: true } : false,
+  ssl: isProd
+    ? {
+        rejectUnauthorized: false, // RAILWAY requires this
+      }
+    : false,
 });
