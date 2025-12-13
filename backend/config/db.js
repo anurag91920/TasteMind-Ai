8a5183f6f0dcc2f7f443a2bc3ff8c28f88487142
Anurag Chaurasiya <anurag9120959628@gmail.com>
2025-12-13 11:21:01 +0530

    add database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,49 +1,21 @@
-import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
-// Optional test (non-blocking)
-db.getConnection()
-  .then(conn => {
-    console.log(" MySQL Connected");
-    conn.release();
-  })
-  .catch(err => {
-    console.error("MySQL Connection Failed:", err.message);
-  });
+import mysql from "mysql2/promise";
 
 
+const isProd = process.env.NODE_ENV === "production";
 
-
-
-
-
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// import mysql from "mysql2/promise";
-
-
-// const isProd = process.env.NODE_ENV === "production";
-
-// export const db = await mysql.createPool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   ssl: isProd
-//     ? {
-//         rejectUnauthorized: false, // RAILWAY requires this
-//       }
-//     : false,
-// });
+export const db = await mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    ssl: isProd
+      ? {
+          rejectUnauthorized: false, 
+       }
+     : false,
+ });
